60d1763a3117ce62b815601e9f74c6e6dfc95663
ahmetws <ahmetyk@gmail.com>
2019-01-02 10:52:06 +0000

    Save data on after save function

--- cloud/main.js
+++ cloud/main.js
@@ -4,25 +4,32 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.afterSave("videos",function(request){
 
-    console.log("after videos");
-    console.log("request: " + JSON.stringify(request, undefined, 2));
+    request.log.info("after videos");
+    request.log.info("request: " + JSON.stringify(request, undefined, 2));
 
-        if (request.object.isNew()){
-            console.log("aftersave videos, object IsNew==true: " + request.object);
+    request.log.info("aftersave videos, object IsNew==true: " + request.object);
 
-            if(request.object.get("youtubeId")) {
-            	console.log("youtubeId: " + request.object.get("youtubeId"));
+    if(request.object.get("youtubeId")) {
+      	request.log.info("youtubeId: " + request.object.get("youtubeId"));
 
-            	var youtubeId = request.object.get("youtubeId");
+      	var youtubeId = request.object.get("youtubeId");
+        
+        if(!request.object.get("image")) {
+       		request.object.set("image", "http://i3.ytimg.com/vi/"+ youtubeId +"/maxresdefault.jpg");
+         }
+      	
+      	if(!request.object.get("url")) {
+ 			request.object.set("url", "https://www.youtube.com/embed/"+ youtubeId +"");
+   		}
+ 
+      }
 
-            	request.object.set("image", "http://i3.ytimg.com/vi/"+ youtubeId +"/maxresdefault.jpg");
-    			request.object.set("url", "https://www.youtube.com/embed/"+ youtubeId +"");
-        	}
+      if(request.object.get("title") && !request.object.get("shortUrl")) {
+      	var title = request.object.get("title").toLowerCase();
+      	var replacedTitle = title.split(' ').join('+');
+      
+        request.object.set("shortUrl", replacedTitle);
+      }
 
-        	if(request.object.get("title")) {
-        		var title = request.object.get("title").toLowerCase();
-        		var replacedTitle = title.split(' ').join('+');
-            	request.object.set("shortUrl", replacedTitle);
-        	}
-        }
+      request.object.save();
 });
