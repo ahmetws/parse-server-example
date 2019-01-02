cfc93118dd4d90cbde3e7b5c6e1a309dabcfaffa
ahmetws <ahmetyk@gmail.com>
2019-01-02 10:34:57 +0000

    Add after save for videos

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,32 @@
 Parse.Cloud.define('hello', function(req, res) {
   return 'Hi';
 });
+
+Parse.Cloud.afterSave("videos",function(request,response){
+
+    console.log("after videos");
+    console.log("request: " + JSON.stringify(request, undefined, 2));
+
+        if (request.object.isNew()){
+            console.log("aftersave videos, object IsNew==true: " + request.object);
+
+            if(request.object.get("youtubeId")) {
+            	console.log("youtubeId: " + request.object.get("youtubeId"));
+
+            	var youtubeId = request.object.get("youtubeId");
+
+            	request.object.set("image", "http://i3.ytimg.com/vi/"+ youtubeId +"/maxresdefault.jpg");
+    			request.object.set("url", "https://www.youtube.com/embed/"+ youtubeId +"");
+        	}
+
+        	if(request.object.get("title")) {
+        		var title = request.object.get("title").toLowerCase();
+        		var replacedTitle = title.split(' ').join('+');
+            	request.object.set("shortUrl", replacedTitle);
+        	}
+
+        }
+
+    response.success();
+
+});
